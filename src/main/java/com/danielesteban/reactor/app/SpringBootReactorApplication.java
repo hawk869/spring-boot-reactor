bc639f89e5652a5a5cc430ea2d94797486b45921
Daniel Esteban <decepeda@misena.edu.co>
2023-07-20 14:32:07 -0500

    Creamos clase Usuario.java e implementamos el operador map con esta clase

--- src/main/java/com/danielesteban/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/danielesteban/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.danielesteban.reactor.app;
 
+import com.danielesteban.reactor.app.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -17,17 +18,22 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Flux<String> nombres = Flux.just("Daniel", "Maria", "Pedro", "Andres", "Juan")
+        Flux<Usuario> nombres = Flux.just("Daniel", "Maria", "Pedro", "Andres", "Juan")
 //                .doOnNext(System.out::println);
-                .doOnNext(e -> {
-                    if (e.isEmpty())
+                .map(nombre -> new Usuario(nombre.toUpperCase(), null))
+                .doOnNext(usuario -> {
+                    if (usuario == null)
                         throw new RuntimeException("Nombres no puede estar vacio");
-                    System.out.println(e);
+                    System.out.println(usuario.getNombre());
                 })
-                .map(String::toUpperCase);
+                .map(usuario -> {
+                    String nombre = usuario.getNombre().toLowerCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
+                });
 
 //        nombres.subscribe(log::info);
-        nombres.subscribe(log::info,
+        nombres.subscribe(usuario -> log.info(usuario.getNombre()),
                 error -> log.error(error.getMessage()),
                 () -> log.info("Ha finalizado la ejecucion del observable con exito!!"));
     }
