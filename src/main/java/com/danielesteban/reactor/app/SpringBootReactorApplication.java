07f25077fbf299d729a5145277e2b08d4a48314c
Daniel Esteban <decepeda@misena.edu.co>
2023-07-20 13:31:02 -0500

    Creando un Strea Flux observable

--- src/main/java/com/danielesteban/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/danielesteban/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,22 @@
 package com.danielesteban.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootReactorApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        Flux<String> nombres = Flux.just("Daniel", "Pedro", "Andres", "Juan")
+                .doOnNext(System.out::println);
+
+        nombres.subscribe();
+    }
 }
